728a85769d4d363feeb31a951a1ab0ddbe2aacd7
tdameff <tdameff@asu.edu>
2018-11-20 16:38:23 -0700

    Added JUnit test for singleton

--- src/test/ApiaryTest.java
+++ src/test/ApiaryTest.java
@@ -4,11 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import singletonPattern.Apiary;
+
 public class ApiaryTest {
 
+    /**
+     * Check to make sure the singleton is working correctly
+     */
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void singleton() {
+        Apiary s1 = Apiary.getInstance();
+        Apiary s2 = Apiary.getInstance();
+        
+        assertEquals(s1, s2);
+        
+        s1 = null;
+        
+        assertNotEquals(s1, s2);
     }
-
 }
