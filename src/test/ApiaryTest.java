87c8fbf2655b0630b6a2f81a6486ff7262036511
tdameff <tdameff@asu.edu>
2018-11-21 19:25:16 -0700

    Added JUnit testing for beehive list

--- src/test/ApiaryTest.java
+++ src/test/ApiaryTest.java
@@ -1,12 +1,15 @@
 package test;
 
 //Imports
+import builderpattern.Beehive;
+import builderpattern.BeehiveRooms;
+import singletonpattern.Apiary;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import singletonpattern.Apiary;
 
 /**
  * Test Class for Apiary.java.
@@ -31,5 +34,17 @@ public class ApiaryTest {
     s1 = null;
 
     assertNotEquals(s1, s2);
+  }
+  
+  @Test
+  public void addBeehive() {
+    Apiary apiary = Apiary.getInstance();
+    BeehiveRooms rooms = apiary.createBeehiveRooms().spawnRooms(2)
+        .commonRooms(4).bedRooms(5).build();
+    Beehive beehive = apiary.createBeehive().id(1).name("BEEHIVE").rooms(rooms).build();
+    
+    apiary.addBeehive(beehive);
+    
+    assertTrue(apiary.getBeehives().get(1).getId() == beehive.getId());
   }
 }
