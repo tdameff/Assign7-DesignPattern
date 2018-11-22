e8bff5708081da4cede9e98fe91f115db54c2166
tdameff <tdameff@asu.edu>
2018-11-21 20:45:28 -0700

    Added JUnit tests for Species class

---
+++ src/builderpattern/Species.java
@@ -0,0 +1,63 @@
+package builderpattern;
+
+public class Species {
+
+  //Variables
+  private String beeSpecies;
+  
+  public Species() {
+    // Defualt species is Bumble Bee
+    this.beeSpecies = "Bumble Bee";
+  }
+  
+  public Species(int species) {
+    
+    if (species == 1) {
+      this.beeSpecies = "Bumble Bee";
+    }
+    else if (species == 2) {
+      this.beeSpecies = "Honey Bee";
+    }
+    else if (species == 3) {
+      this.beeSpecies = "Carpenter Bee";
+    }
+    else if (species == 4) {
+      this.beeSpecies = "Andrena Bee";
+    }
+    else if (species == 5) {
+      this.beeSpecies = "Apidae Bee";
+    }
+  }
+
+  /**
+   * Get the bee species
+   * 
+   * @return the beeSpecies
+   */
+  public String getBeeSpecies() {
+    return beeSpecies;
+  }
+
+  /**
+   * Set the bee species
+   * 
+   * @param beeSpecies the beeSpecies to set
+   */
+  public void setBeeSpecies(int species) {
+    if (species == 1) {
+      this.beeSpecies = "Bumble Bee";
+    }
+    else if (species == 2) {
+      this.beeSpecies = "Honey Bee";
+    }
+    else if (species == 3) {
+      this.beeSpecies = "Carpenter Bee";
+    }
+    else if (species == 4) {
+      this.beeSpecies = "Andrena Bee";
+    }
+    else if (species == 5) {
+      this.beeSpecies = "Apidae Bee";
+    }
+  }
+}
