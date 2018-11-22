adb9b5d6874d705fc90345c55a34f6c0c669fd8d
tdameff <tdameff@asu.edu>
2018-11-21 19:49:57 -0700

    Added new JUnit tests

--- src/test/BeehiveRoomsTest.java
+++ src/test/BeehiveRoomsTest.java
@@ -4,11 +4,61 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import builderpattern.Beehive;
+import builderpattern.BeehiveRooms;
+import builderpattern.BeehiveRooms.RoomBuilder;
+import singletonpattern.Apiary;
+
 public class BeehiveRoomsTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void BuilderTest() {
+    Apiary apiary = Apiary.getInstance();
+    
+    BeehiveRooms rooms = apiary.createBeehiveRooms()
+        .spawnRooms(2).commonRooms(4).bedRooms(8).build();
+    
+    Beehive hive = apiary.createBeehive().id(1).name("HIVE")
+        .rooms(rooms).build();
+    
+    assertTrue(hive.getId() == 1);
+    assertTrue(hive.getName() == "HIVE");
+    assertTrue(hive.getRooms().getSpawnRooms() == 2);
+    assertTrue(hive.getRooms().getCommonRooms() == 4);
+    assertTrue(hive.getRooms().getBedRooms() == 8);
+    
+    assertTrue(rooms.getSpawnRooms() == 2);
+    assertTrue(rooms.getCommonRooms() == 4);
+    assertTrue(rooms.getBedRooms() == 8);
+  }
+  
+  @Test
+  public void setTest() {
+    Apiary apiary = Apiary.getInstance();
+    
+    BeehiveRooms rooms = apiary.createBeehiveRooms()
+        .spawnRooms(2).commonRooms(4).bedRooms(8).build();
+    
+    Beehive hive = apiary.createBeehive().id(1).name("HIVE")
+        .rooms(rooms).build();
+    
+    assertTrue(hive.getId() == 1);
+    assertTrue(hive.getName() == "HIVE");
+    assertTrue(hive.getRooms().getSpawnRooms() == 2);
+    assertTrue(hive.getRooms().getCommonRooms() == 4);
+    assertTrue(hive.getRooms().getBedRooms() == 8);
+    
+    hive.setId(5);
+    hive.setName("BEEE HIVE");
+    rooms.setSpawnRooms(6);
+    rooms.setCommonRooms(3);
+    rooms.setBedRooms(2);
+    
+    assertTrue(hive.getId() == 5);
+    assertTrue(hive.getName() == "BEEE HIVE");
+    assertTrue(hive.getRooms().getSpawnRooms() == 6);
+    assertTrue(hive.getRooms().getCommonRooms() == 3);
+    assertTrue(hive.getRooms().getBedRooms() == 2);
   }
 
 }
--- src/test/BeehiveTest.java
+++ src/test/BeehiveTest.java
@@ -1,17 +1,24 @@
 package test;
 
 //Imports
+import builderpattern.Beehive;
+import singletonpattern.Apiary;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class BeehiveTest {
 
   @Test
-  public void test() {
-    fail("Not yet implemented");
+  public void beehiveTest() {
+    Apiary apiary = Apiary.getInstance();
+    
+    Beehive hive = apiary.createBeehive().id(1).name("BEEHIVE").build();
+    
+    assertTrue(hive.getId() == 1);
+    assertTrue(hive.getName() == "BEEHIVE");
+    
   }
-
 }
