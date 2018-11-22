ef2b50547da9c60b18006f39f2dfae939794dbf5
tdameff <tdameff@asu.edu>
2018-11-21 21:36:42 -0700

    Added getBeehive and getStrenth methods

---
+++ src/decoratorpattern/Bee.java
@@ -0,0 +1,15 @@
+package decoratorpattern;
+
+import builderpattern.Species;
+
+public interface Bee {
+
+  //Get the species of the Bee
+  public String getSpecies();
+  
+  public String getBeehive();
+  
+  public String getJob();
+  
+  public double getStrength();
+}
